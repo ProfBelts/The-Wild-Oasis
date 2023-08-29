839afceb99cb8705ce56e3426f91000979149801
Joseph Beltran <profbelts@gmail.com>
2023-08-29 09:05:38 +0800

    Build reusable components

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 function App() {
   const StyledApp = styled.main`
-    background-color: orangered;
     padding: 20px;
   `;
 
@@ -15,11 +15,27 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">Hello World</Heading>
-        <Button> Check In </Button>
-        <Heading as="h2">Hello World</Heading>
-        <Heading as="h3">Hello World</Heading>
-        <Input type="number" placeholder="Number of Guests"></Input>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+
+            <div>
+              <Heading as="h2">Check in and Out</Heading>
+              <Button> Check In </Button>
+              <Button variation="secondary" size="small">
+                {" "}
+                Check Out{" "}
+              </Button>
+            </div>
+          </Row>
+          <Row type="vertical">
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of Guests"></Input>
+              <Input type="number" placeholder="Number of Guests"></Input>
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
